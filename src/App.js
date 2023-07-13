7cf24bf4513adce220713451dd55686497bb4c0b
Rajiv Kumar Gupta <100814533+rajivgupta1@users.noreply.github.com>
2023-07-13 12:54:58 +1000

    login_submit

--- src/App.js
+++ src/App.js
@@ -2,29 +2,112 @@ import React, { useState } from "react";
 import './hello.css';
 
 
-const App = () => {
-  const purple = "orange";
-   
-  const [bg, setBg] = useState(purple);
-  const [name, setName] = useState("click me");
+const App = () =>{
+   const [fullname, setFullName] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    phone: ''
+   });
   
 
-   const bgChange = () =>{
-    let newBg = "#34495e";
-      setBg(newBg);
-      setName("Ouch!! ");
+   const inputEvent = (event) =>{
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    // const value = event.target.value;
+    // const name = event.target.name;
+
+    const {value, name} = event.target;
+
+    setFullName((preValue) =>{
+      console.log(preValue);
+      if(name === "fname"){
+       return { fname: value,
+        lname: preValue.lname,
+        email: preValue.email,
+        phone: preValue.phone
+       };
+      }else if(name === "lname"){
+        return { 
+         fname: preValue.fname,
+         lname: value,
+         email: preValue.email,
+         phone: preValue.phone
+        }
+      }else if(name === "email"){
+        return { 
+         fname: preValue.fname,
+         lname: preValue.lname,
+         email: value,
+         phone: preValue.phone
+        }
+      }else if(name === "phone"){
+        return { 
+         fname: preValue.fname,
+         lname: preValue.lname,
+         email: preValue.email,
+         phone: value
+        }
+      }
+
+    });
    };
 
-   const bgBack = () =>{
-    setBg(purple);
-    setName("Ayyo!!");
-   }
-  
-  return (
-    <div style={{backgroundColor: bg }}>
+   const onSubmit = (event) =>{
+       event.preventDefault();
+       alert("form submitted");
+      }
 
-    <button onClick={bgChange} onDoubleClick={bgBack} className="btn">{name}</button>
+   
 
+  return(
+
+    <div className="main_dev">
+    <form onSubmit={onSubmit}>
+     <div>
+    <h1>Hello {fullname.fname} {fullname.lname} </h1>
+    <p>{fullname.email}</p>
+
+    <p> {fullname.phone}</p>
+    <input type="text"
+     placeholder="Enter your first name"
+     name="fname"
+      onChange={inputEvent}
+      value={fullname.fname}
+     />
+<br />
+<input type="text"
+     placeholder="Enter your last name"
+     name="lname"
+      onChange={inputEvent}
+      value={fullname.lname}
+     />
+
+<br/>
+<input type="email"
+     placeholder="Enter your Email-address"
+     name="email"
+      onChange={inputEvent}
+      value={fullname.email}
+     />
+    <br />
+    <input type="number"
+     placeholder="Enter your phone Number"
+     name="phone"
+      onChange={inputEvent}
+      value={fullname.phone}
+     />
+
+
+
+    <br/>
+    <button type="submit" className='btn'>
+      Submit Me
+    </button>
+
+    </div>
+    </form>
     </div>
   )
 }
